75844db3cc741b33be78beafa2f50f38367ea873
jinuland <jinwoo.park@navercorp.com>
2018-09-16 19:34:51 +0900

    initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "첫글",
+        "excerpt":"오늘 처음으로 블로그를 생성했다. 앞으로 여기에 여러가지 글을 올리려한다. ","categories": ["life"],
+        "tags": [],
+        "url": "http://localhost:4000/life/first-post/",
+        "teaser":null}]
